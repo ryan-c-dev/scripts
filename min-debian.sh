86594e9c2b69e18b44de05d91c71401cf1742637
Ryan Considine <contact@ryanconsidine.com>
2022-04-12 02:54:45 -0700

    minimize debian server install, these packages not needed

---
+++ min-debian.sh
@@ -0,0 +1,2 @@
+apt remove --purge acpi acpid aptitude at aspell aspell-en avahi-daemon bc bin86 bind9-host console-common console-data console-tools dc
+debian-faq debian-faq-de debian-faq-fr debian-faq-it debian-faq-zh-cn doc-debian doc-linux-text eject fdutils finger foomatic-filters gettext-base hplip iamerican ibritish ispell laptop-detect libavahi-compat-libdnsd1 libgpmg1 mtools mtr-tiny netcat net-tools pidentd ppp pppconfig pppoe pppoeconf read-edid reportbug smclient tcsh usbutils vim-common vim-tiny warmerican w3m whois zeroinstall-injector
